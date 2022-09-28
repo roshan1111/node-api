bf487836fd7f8d9487d4fcf022fb432e0044cbcb
roshan <roshansubedi@Roshans-MacBook-Pro.local>
2022-09-28 23:22:08 +0200

    api

---
+++ data.js
@@ -0,0 +1,16 @@
+let countries = [
+    {
+        id: 1,
+        name: "Australia"
+    },
+    {
+        id: 2,
+        name: "Nepal"
+    },
+    {
+        id: 3,
+        name: "Denmark"
+    },
+];
+
+module.exports = countries
--- index.js
+++ index.js
@@ -1,32 +1,86 @@
 const http = require("http");
+const qs = require("querystring");
 const fs = require("fs");
+const port = 3004;
+let countries = require("./data");
+// console.log(countries)
 
-const hostname = '127.0.0.1';
-const port = process.env.port;
-const home = fs.readFileSync("./home.html");
-const about = fs.readFileSync("./about.html");
-
-
-const server = http.createServer((req, res)=>{
-    console.log(req.url);
-    url = req.url;
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    if(url == '/'){
-        res.end(home);
+const server = http.createServer((req, res) => {
+  let url = req.url;
+  if (url === "/" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>Hello World </h1>");
+    res.end();
+  } else if (url === "/api/countries" && req.method === "GET") {
+    try {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify(countries));
+      res.end();
+    } catch (err) {
+      console.log(err);
     }
-    else if(url == '/about'){
-        res.end(about);
+  } else if (url.match(/\/api\/countries\/([0-9])+/) && req.method === "GET") {
+    try {
+      const requestUrl = url.split("/");
+      const id = requestUrl[3];
+      // console.log(id)
+      const country = countries.find((country) => country.id === Number(id));
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify(country));
+      res.end();
+    } catch (err) {
+      console.log(err);
     }
-    
-    
-    else{
-        res.statusCode = 404;
-        res.end("<h1>404 not found</h1>");
+  } else if (
+    url.match(/\/api\/countries\/([0-9])+/) &&
+    req.method === "DELETE"
+  ) {
+    try {
+      const requestUrl = url.split("/");
+      const id = requestUrl[3];
+      // console.log(id)
+      countries = countries.filter((country) => country.id !== Number(id));
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify(countries));
+      res.end();
+    } catch (err) {
+      console.log(err);
     }
+  } else if (url === "/api/countries" && req.method === "POST") {
+    try {
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+
+      req.on("end", () => {
+        // console.log(qs.parse(body))
+        let countryName = qs.parse(body);
+        let newCountry = {
+          id: Math.floor(4 + Math.random() * 1000),
+          name: countryName["name"],
+        };
+        countries.push(newCountry);
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.write(JSON.stringify(countries));
+        res.end();
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  } 
+  
+  else {
+    try {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify({ message: "file not found" }));
+      res.end();
+    } catch (err) {
+      console.log(err);
+    }
+  }
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-}); 
+server.listen(port, () => {
+  console.log(`server is running at http://localhost:${port}/`);
+});
